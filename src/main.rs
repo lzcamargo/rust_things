582fe5b4ea458ee822b6bedbbebbfc47679f5a2b
camargho <camargho@gmail.com>
2025-03-24 18:24:46 +0000

    atualiza02

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+  let user_id = 12345;
+  get_username(user_id);
 }
+
+  fn get_username(user_id: i32) {
+    println!("Informe o nome :");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Leitura falhou");
+    println!("Código do usuário: {}, Nome: {}", user_id, input);
+    
+  }  
+
+  
